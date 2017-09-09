229b55be06f087eeabc39de1292bab07e143507a
Justin Tooley <jstooley823@gmail.com>
2017-09-09 02:05:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  vowel = false
+  if word.match(/^[aeiou]/i)
+    vowel = true
+  end
+  vowel
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b(un)|(ing)\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +19,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  bool = false
+  if phone.match(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
+    bool = true
+  end
+  bool
 end
